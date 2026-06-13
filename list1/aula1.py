208007a7e75d286d194620b8fdb5193d72bb4f9b
leonardomozart <128268048+Leonardo-mozart@users.noreply.github.com>
2026-06-13 11:47:29 -0300

    Add print statements to aula1.py

--- list1/aula1.py
+++ list1/aula1.py
@@ -1,3 +1,4 @@
 print(12, 34, sep="-", end="\n") # Aqui n acontece nd pq já vem o \n no final da frase
 print(27, 15, sep='-', end="##") # End colocando um caracter sem o \n, vai retira o \n, pra quebrar a linha coloque  
 print(28, 19, sep='-')
+print('Teste para o GitHub')
